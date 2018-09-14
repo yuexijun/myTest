8142e283628e31b5f8dc8f04de9c7ea1ae690acf
yuexijun <yuexj_leon@163.com>
2018-09-14 14:57:32 +0800

    添加了一行注释

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println(System.currentTimeMillis());
+		//添加一行注释
 		System.out.println("desktopGit");
 	}
 
