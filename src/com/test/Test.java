d8fc0e6f95c1efa3eb2a92fd6bc7245c78eb51c6
yuexijun <yuexj_leon@163.com>
2018-09-14 15:19:46 +0800

    添加注释 打印desktopGit

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println(System.currentTimeMillis());
-		//添加一行注释
+		//添加一行注释   打印desktopGit
 		System.out.println("desktopGit");
 	}
 
